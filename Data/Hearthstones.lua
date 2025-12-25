163ea96dbd613237d301541bce6c8a64c7255399
Imaginary-Narwhal <dragonflyy69@gmail.com>
2025-12-25 14:14:53 -0500

    Fixed item id for Redeployment Module

--- Data/Hearthstones.lua
+++ Data/Hearthstones.lua
@@ -7,36 +7,5 @@ https://www.wowhead.com/items?filter=216:107;1:0;0:hearthstone+location
 
 
 L.HearthstoneIds = {
-    54452,  -- Ethereal Portal
-    64488,  -- The Innkeeper's Daughter
-    93672,  -- Dark Portal
-    142542, -- Tome of Town Portal
-    162973, -- Greatfather Winter's Hearthstone
-    163045, -- Headless Horseman's Hearthstone
-    165669, -- Lunar Elder's Hearthstone
-    165670, -- Peddlefeet's Lovely Hearthstone
-    165802, -- Noble Gardener's Hearthstone
-    166746, -- Fire Eater's Hearthstone
-    166747, -- Brewfest Reveler's Hearthstone
-    168907, -- Holographic Digitalization Hearthstone
-    246565, -- Cosmic Hearthstone
-    172179, -- Eternal Traveler's Hearthstone
-    180290, -- Night Fae Hearthstone
-    182773, -- Necrolord Hearthstone
-    183716, -- Venthyr Sinstone
-    184353, -- Kyrian Hearthstone
-    188952, -- Dominated Hearthstone
-    190196, -- Enlightened Hearthstone
-    190237, -- Broker Translocation Matrix
-    193588, -- Timewalker's Hearthstone
-    200630, -- Ohn'ir Windsage's Hearthstone
-    206195, -- Path of the Naaru
-    208704, -- Deepdweller's Earthen Hearthstone
-    209035, -- Hearthstone of the Flame
-    210455, -- Draenic Hologem
-    212337, -- Stone of the Hearth
-    228940, -- Notorious Thread's Hearthstone
-    236687, -- Explosive Hearthstone
-    246565, -- Redeployment Module
-    245970, -- P.O.S.T. Master's Express Hearthstone
+    162973, 246565, 208704, 206195, 168907, 235016, 190196, 54452, 263489, 64488, 193588, 245970, 200630, 228940, 210455, 93672, 172179, 183716, 188952, 165802, 190237, 209035, 212337, 142542, 236687, 165669, 184353, 182773, 163045, 180290, 166746, 165670, 166747
 }
